efac39104f99152da3a4c11d71b8d149a727497a
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-22 22:47:06 +0100

    Add flake.nix for pre-commit hooks

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "ha-prometheus-sensor";
+
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, pre-commit-hooks, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              black.enable = true;
+              isort.enable = true;
+              ruff.enable = true;
+            };
+          };
+        };
+        devShells.default = with nixpkgs.legacyPackages.${system}; mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+
+          buildInputs = [
+            black
+            isort
+            mypy
+            ruff
+          ] ++ (with python3.pkgs; [
+            # typing stubs
+            aiohttp
+          ]);
+        };
+      }
+    );
+}
