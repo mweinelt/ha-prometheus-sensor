3ecf0839add5540dd9c6e8d9ba470b996e365931
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-22 20:00:04 +0100

    Add typing hints

--- custom_components/prometheus_sensor/sensor.py
+++ custom_components/prometheus_sensor/sensor.py
@@ -1,41 +1,45 @@
 """Prometheus Sensor component."""
 from datetime import timedelta
 import logging
-from typing import Union
+from typing import Dict, Final
 from urllib.parse import urljoin
 
+import aiohttp
 import voluptuous as vol
 
 from homeassistant.components.sensor import (
-    DEVICE_CLASSES_SCHEMA,
     PLATFORM_SCHEMA as SENSOR_PLATFORM_SCHEMA,
-    STATE_CLASSES_SCHEMA,
     SensorEntity,
+)
+from homeassistant.components.sensor.const import (
+    DEVICE_CLASSES_SCHEMA,
+    STATE_CLASSES_SCHEMA,
 )
 from homeassistant.const import (
     CONF_DEVICE_CLASS,
     CONF_NAME,
+    CONF_UNIQUE_ID,
     CONF_UNIT_OF_MEASUREMENT,
     CONF_URL,
-    CONF_UNIQUE_ID,
     STATE_PROBLEM,
     STATE_UNKNOWN,
 )
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType, StateType
 from homeassistant.util import Throttle
 
-_LOGGER = logging.getLogger(__name__)
+_LOGGER: Final = logging.getLogger(__name__)
 
-DEFAULT_URL = "http://localhost:9090"
-CONF_QUERIES = "queries"
-CONF_EXPR = "expr"
-CONF_STATE_CLASS = "state_class"
-MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
+DEFAULT_URL: Final = "http://localhost:9090"
+CONF_QUERIES: Final = "queries"
+CONF_EXPR: Final = "expr"
+CONF_STATE_CLASS: Final = "state_class"
+MIN_TIME_BETWEEN_UPDATES: Final = timedelta(seconds=60)
 
-_LOGGER = logging.getLogger(__name__)
-
-_QUERY_SCHEMA = vol.Schema(
+_QUERY_SCHEMA: Final = vol.Schema(
     {
         vol.Required(CONF_NAME): cv.string,
         vol.Optional(CONF_UNIQUE_ID): cv.string,
@@ -46,7 +50,7 @@ _QUERY_SCHEMA = vol.Schema(
     }
 )
 
-PLATFORM_SCHEMA = SENSOR_PLATFORM_SCHEMA.extend(
+PLATFORM_SCHEMA: Final = SENSOR_PLATFORM_SCHEMA.extend(
     {
         vol.Optional(CONF_URL, default=DEFAULT_URL): cv.string,
         vol.Required(CONF_QUERIES): [_QUERY_SCHEMA],
@@ -54,30 +58,33 @@ PLATFORM_SCHEMA = SENSOR_PLATFORM_SCHEMA.extend(
 )
 
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+async def async_setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    async_add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None
+):
     """Set up the sensor platform."""
     session = async_get_clientsession(hass)
+    url = config[CONF_URL]
+    prometheus = Prometheus(url, session)
 
-    prometheus = Prometheus(config.get(CONF_URL), session)
-
-    sensors = []
-    for query in config.get("queries", {}):
-        sensors.append(PrometheusSensor(prometheus, query))
-
-    _LOGGER.debug("Setting up %d prometheus sensors", len(sensors))
-
-    async_add_entities(sensors, update_before_add=True)
+    queries = config[CONF_QUERIES]
+    async_add_entities(
+        [PrometheusSensor(prometheus, query) for query in queries],
+        update_before_add=True
+    )
 
 
 class Prometheus:
     """Wrapper for Prometheus API Requests."""
 
-    def __init__(self, url: str, session) -> None:
+    def __init__(self, url: str, session: aiohttp.ClientSession) -> None:
         """Initialize the Prometheus API wrapper."""
         self._session = session
         self._url = urljoin(f"{url}/", "api/v1/query")
 
-    async def query(self, expr) -> Union[str, float]:
+    async def query(self, expr: str) -> float | StateType:
         """Query expression response."""
         response = await self._session.get(self._url, params={"query": expr})
         if response.status != 200:
@@ -111,12 +118,12 @@ class Prometheus:
 class PrometheusSensor(SensorEntity):
     """Sensor entity representing the result of a PromQL expression."""
 
-    def __init__(self, prometheus: Prometheus, query: dict) -> None:
+    def __init__(self, prometheus: Prometheus, query: Dict[str, str]) -> None:
         """Initialize the sensor."""
-        self._expr = query.get(CONF_EXPR)
+        self._expr = query[CONF_EXPR]
         self._prometheus: Prometheus = prometheus
 
-        self._attr_name = query.get(CONF_NAME)
+        self._attr_name = query[CONF_NAME]
         self._attr_unique_id = query.get(CONF_UNIQUE_ID)
         self._attr_native_unit_of_measurement = query.get(CONF_UNIT_OF_MEASUREMENT)
         self._attr_state_class = query.get(CONF_STATE_CLASS)
