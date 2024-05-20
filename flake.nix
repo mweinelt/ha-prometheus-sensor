b9319b256af5f426fe1c6ecec6669b55a289c01c
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-05-20 14:52:43 +0200

    flake.nix: make git-hooks nixpkgs instance follow

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
